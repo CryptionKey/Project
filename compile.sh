e743c221c0a2c3075c501a0776a45250ec7e86d7
cry_cry <jean.malouda@yahoo.fr>
2018-11-21 13:44:41 +0100

    Enlevage des erreurs ++

--- compile.sh
+++ compile.sh
@@ -1,13 +1,8 @@
-COMPILATION
-javac -d tp0/bin tp0/src/*.java
+//COMPILATION
+/javac -d tp0/bin tp0/src/*.java
 
 //pour les depéndances
 javac -cp tp0/bin -d tp1/bin tp1/src/*.java
 
 
-
-EXECUTION
-java -cp "tp0/bin:tp1/bin" BlackJackConsole
-	ou
-java -cp "tp0/bin;tp1/bin" BlackJackConsole
  
--- restaurant/src/restaurant/Clients.java
+++ restaurant/src/restaurant/Clients.java
@@ -20,7 +20,7 @@ public class Clients {
     //Afficher la liste des notes de client
     void afficherListe(){
         for (Note note : this.noteList) {
-            logger.info("", "\t"+note.getNom()+"\n");
+            logger.info("OUTPUT", "\t"+note.getNom()+"\n");
         }
     }
 
@@ -49,61 +49,61 @@ public class Clients {
 
     Note ouvrir_note(Clients clients){
         if(clients.getNoteList().size()!=0){
-            logger.info("","Les clients actuellement dans le restaurant sont:\n");
+            logger.info("OUTPUT","Les clients actuellement dans le restaurant sont:\n");
             afficherListe();}
-        logger.info("","\nDonnez le nom du client dont vous souhaitez ouvrir ou créer la note: ");
-        String nom_client = scan.next();
+        logger.info("OUTPUT","\nDonnez le nom du client dont vous souhaitez ouvrir ou créer la note: ");
+        String nom_client = scan.next();logger.info("INPUT","\tNote du client demandée: "+nom_client+".\t");
         Note note = null;
         if(!verification_client_existant(nom_client)) {
             note = new Note(nom_client);
             clients.add(note);
-            logger.info("","Nouvelle note créée\n");
+            logger.info("OUTPUT","Nouvelle note créée\n");
         }
         else {
             if(clients.noteList.get(getIndexNote(nom_client)).getProductList().size()!=0) {
-                logger.info("", "Voici la note demandée:\n");
+                logger.info("OUTPUT", "Voici la note demandée:\n");
                 clients.noteList.get(getIndexNote(nom_client)).afficherListe();
             }
-            else{ logger.info("","Cette note ne contient encore aucun produit.\n");}
+            else{ logger.info("OUTPUT","Cette note ne contient encore aucun produit.\n");}
         }
         return note;
     }
 
     void enregistrer(Clients clients, Products products){
-        logger.info("","Entrez le nom du client que vous voulez facturer:");
-        String nom_client = scan.next();
+        logger.info("OUTPUT","Entrez le nom du client que vous voulez facturer:");
+        String nom_client = scan.next(); logger.info("INPUT","\tNom du client demandé: "+nom_client+".\n");
         if(verification_client_existant(nom_client)) {
-            logger.info("","Entrez le nom de l'article que vous souhaitez facturer:");
-            String nom_aliment = scan.next();
+            logger.info("OUTPUT","Entrez le nom de l'article que vous souhaitez facturer:");
+            String nom_aliment = scan.next(); logger.info("INPUT","\tNom de l'aliment demandé: "+nom_aliment+".\n");
             if(products.verification_aliment_existant(nom_aliment)) {
-                logger.info("","Entrez la quantité du produit souhaité:");
-                int quantite = scan.nextInt();
+                logger.info("OUTPUT","Entrez la quantité du produit souhaité:");
+                int quantite = scan.nextInt();logger.info("INPUT","\tQuantité demandée: "+quantite+".\n");
                 int index_aliment = products.getIndexAliment(nom_aliment);
                 Aliment aliment_demande = products.getProductList().get(index_aliment);
-                if(quantite>aliment_demande.getQuantite()){ logger.info("", "Il n'y en a pas assez en stock\n"); }
+                if(quantite>aliment_demande.getQuantite()){ logger.info("OUTPUT", "Il n'y en a pas assez en stock\n"); }
                 else{ Aliment aliment = new Aliment(nom_aliment.toLowerCase(), quantite, products.getProductList().get(index_aliment).getPrix());
                     clients.getNoteList().get(getIndexNote(nom_client)).getProductList().add(aliment);
                     if(!"café".equals(aliment_demande.getNom())){ products.getProductList().get(index_aliment).setQuantite(aliment_demande.getQuantite()-quantite);
                     if(aliment_demande.getQuantite()==0){ products.getProductList().remove(products.getProductList().get(index_aliment)); } } }
-            } else{logger.info("","Ce produit n'est pas proposé à la vente.\n");}
-        } else {logger.info("", "Ce client n'existe pas, ouvrez d'abord une nouvelle note (entrez n)\n"); }
+            } else{logger.info("OUTPUT","Ce produit n'est pas proposé à la vente.\n");}
+        } else {logger.info("OUTPUT", "Ce client n'existe pas, ouvrez d'abord une nouvelle note (entrez n)\n"); }
     }
 
     void cloturer(Clients clients){
-        logger.info("","\nEntrez le nom du client dont vous voulez fermez la note: ");
-        String nom_client = scan.next();
+        logger.info("OUTPUT","\nEntrez le nom du client dont vous voulez fermez la note: ");
+        String nom_client = scan.next();logger.info("INPUT","\tNom du client : "+nom_client+".\n");
         int remise = 0;
         if(verification_client_existant(nom_client)) {
             String choix;
             do {
-                logger.info("", "Souhaitez-vous offrir une remise de 10% à ce client? [o : oui / n : non]");
-                choix = scan.next();
+                logger.info("OUTPUT", "Souhaitez-vous offrir une remise de 10% à ce client? [o : oui / n : non]");
+                choix = scan.next();logger.info("INPUT","Choix: "+choix+".\n");
             }while(!"o".equals(choix) && !"n".equals(choix));
             if("o".equals(choix)){ remise = 1;}
             int index_note = getIndexNote(nom_client);
             clients.noteList.get(index_note).facture(remise);
             clients.getNoteList().remove(clients.getNoteList().get(index_note));
-        } else {logger.info("", "Ce client n'existe pas, ouvrez d'abord une nouvelle note (entrez n)\n"); }
+        } else {logger.info("OUTPUT", "Ce client n'existe pas, ouvrez d'abord une nouvelle note (entrez n)\n"); }
     }
 
 }

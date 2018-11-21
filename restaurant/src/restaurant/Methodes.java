d802e85fbe396a162eb3f1a3bc89d1db65138d3a
BackInByte <jean66.jean@laposte.net>
2018-11-21 13:52:02 +0100

    Merge branch 'master' of C:\Users\Danielle\IdeaProjects\Project with conflicts.

--- restaurant/src/restaurant/Methodes.java
+++ restaurant/src/restaurant/Methodes.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 import logger.Logger;
 import logger.LoggerFactory;
 
-class Methodes {
+public class Methodes {
 
     private Logger logger = LoggerFactory.getLogger("methodes");
+    private Scanner scan = new Scanner(System.in); Scanner scan1 = new Scanner(System.in); Scanner scan2 = new Scanner(System.in);
 
-    void affichage_choix() {
-        logger.info("", "\to: Afficher la liste des opérations disponibles.\n");
-        logger.info("", "\tp: Afficher la liste des produits à la vente.\n");
-        logger.info("", "\ta: Ajouter un produit à la vente.\n");
-        logger.info("", "\tn: Ouvrir la note d'un client.\n");
-        logger.info("", "\tk: Afficher la liste des clients\n");
-        logger.info("", "\te: Enregistrer la vente d'un produit sur la note d'un client.\n");
-        logger.info("", "\tf: Clotûrer la note d'un client en affichant:\n");
-        logger.info("", "\t\tle prix de chaque produit HT (hors-taxe),\n\t\tle prix total HT,\n");
-        logger.info("", "\t\tla TVA(10%),\n\t\tle prix TTC.\n");
-        logger.info("", "\tc: Afficher les données comptables:\n");
-        logger.info("", "\t\ttotal des rentrés d'argent,\n\t\ttotal de la TVA facturée.\n");
-        logger.info("", "\tq: Quitter le restaurant.\n");
+    public void affichage_choix(){
+        logger.info("","\to: Afficher la liste des opérations disponibles.\n" );
+        logger.info("","\tp: Afficher la liste des produits à la vente.\n");
+        logger.info("","\ta: Ajouter un produit à la vente.\n");
+        logger.info("","\tn: Ouvrir la note d'un client.\n");
+        logger.info("","\tk: Afficher la liste des clients\n");
+        logger.info("","\te: Enregistrer la vente d'un produit sur la note d'un client.\n");
+        logger.info("","\tf: Clotûrer la note d'un client en affichant:\n");
+        logger.info("","\t\tle prix de chaque produit HT (hors-taxe),\n\t\tle prix total HT,\n");
+        logger.info("","\t\tla TVA(10%),\n\t\tle prix TTC.\n");
+        logger.info("","\tc: Afficher les données comptables:\n");
+        logger.info("","\t\ttotal des rentrés d'argent,\n\t\ttotal de la TVA facturée.\n");
+        logger.info("","\tq: Quitter le restaurant.\n");
     }
 
 }

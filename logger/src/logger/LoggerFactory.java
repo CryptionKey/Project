5f7726d1daa3e4163243825303d522ac911b64ff
cry_cry <jean.malouda@yahoo.fr>
2018-11-25 18:02:34 +0100

    Merge branch 'master' of C:\Users\anael\POO\Project15-11 with conflicts.

--- logger/src/logger/LoggerFactory.java
+++ logger/src/logger/LoggerFactory.java
@@ -4,8 +4,9 @@ package logger;
 public class LoggerFactory {
 
     public static Logger getLogger(String name) {
-        return new CompositeLogger(new ContextualLogger(name, new FileLogger("./loggerOutput.txt")),  new ConsoleLogger());
-        // return new CompositeLogger(new ContextualLogger(name, new FilteredLogger(new FileLogger("./loggerOutput.txt"), message -> !message.contains("play"))),new ConsoleLogger());
+        //return new CompositeLogger(new ContextualLogger(name, new FileLogger("./loggerOutput.txt")),  new ConsoleLogger());
+        return new CompositeLogger(new ContextualLogger(name, new FilteredLogger(new FileLogger("./loggerOutput.txt"), message -> !message.contains("play"))),
+                                   new ConsoleLogger(name, new FilteredLogger(new FileLogger("./loggerOutput.txt"), message -> !message.contains("Vous"))));
     }
 
 }
